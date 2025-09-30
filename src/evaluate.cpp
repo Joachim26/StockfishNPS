f30fcf08ead60aa7f135495f5f28e5d129449a4a
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2025-09-30 20:00:30 +0200

    Never re-evaluate

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -73,13 +73,15 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     Value nnue = (125 * psqt + 131 * positional) / 128;
 
     // Re-evaluate the position when higher eval accuracy is worth the time spent
+    /* Never re-evaluate
     if (smallNet && (std::abs(nnue) < 236))
     {
         std::tie(psqt, positional) = networks.big.evaluate(pos, accumulators, &caches.big);
         nnue                       = (125 * psqt + 131 * positional) / 128;
         smallNet                   = false;
     }
-
+    */
+  
     // Blend optimism and eval with nnue complexity
     int nnueComplexity = std::abs(psqt - positional);
     optimism += optimism * nnueComplexity / 468;
